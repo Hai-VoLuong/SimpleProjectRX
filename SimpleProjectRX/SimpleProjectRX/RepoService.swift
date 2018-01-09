91283cc8f864b18f3e7e14d816290e17f693fde1
HaiVL <hai.softech@gmail.com>
2018-01-09 11:59:27 +0700

    Create RepoService

--- SimpleProjectRX/SimpleProjectRX/RepoService.swift
+++ SimpleProjectRX/SimpleProjectRX/RepoService.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2018 Hai Vo L. All rights reserved.
 //
 
-import Foundation
+import RxSwift
+
+protocol RepoServiceProtocol {
+    func repoList(input: RepoListInput) -> Observable<RepoListOutput>
+    func eventList(input: EventListInput) -> Observable<EventListOutput>
+}
+
+class RepoService: APIService, RepoServiceProtocol {
+
+    func repoList(input: RepoListInput) -> Observable<RepoListOutput> {
+        return self.request(input)
+        .observeOn(MainScheduler.instance)
+        .shareReplay(1)
+    }
+
+    func eventList(input: EventListInput) -> Observable<EventListOutput> {
+        return self.requestArray(input)
+        .observeOn(MainScheduler.instance)
+        .map { events -> EventListOutput in
+            return EventListOutput(events: events)
+        }
+        .shareReplay(1)
+    }
+}
