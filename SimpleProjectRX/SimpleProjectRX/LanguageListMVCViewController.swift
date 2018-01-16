e3536420a772efd4edfd5e683492d3064bb6f861
HaiVL <hai.softech@gmail.com>
2018-01-16 14:21:15 +0700

    Create LanguageListMVCViewController

---
+++ SimpleProjectRX/SimpleProjectRX/LanguageListMVCViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LanguageListMVCViewController.swift
+//  SimpleProjectRX
+//
+//  Created by Hai Vo L. on 1/16/18.
+//  Copyright © 2018 Hai Vo L. All rights reserved.
+//
+
+import UIKit
+
+class LanguageListMVCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "LanguageListMVC"
+    }
+}
