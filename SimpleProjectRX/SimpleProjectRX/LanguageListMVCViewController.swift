cc5766914fb309acd1a2cea4c93655d2baeb8e24
HaiVL <hai.softech@gmail.com>
2018-01-16 15:19:11 +0700

    update project

--- SimpleProjectRX/SimpleProjectRX/LanguageListMVCViewController.swift
+++ SimpleProjectRX/SimpleProjectRX/LanguageListMVCViewController.swift
@@ -8,10 +8,28 @@
 
 import UIKit
 
-class LanguageListMVCViewController: UIViewController {
+final class LanguageListMVCViewController: UIViewController {
+
+    @IBOutlet private weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "LanguageListMVC"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 }
+
+extension LanguageListMVCViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+}
+
+extension LanguageListMVCViewController: UITableViewDelegate {}
