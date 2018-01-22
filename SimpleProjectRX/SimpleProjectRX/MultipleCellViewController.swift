a2a88ac095a6fd6c01185b85c3b7b5d575b1c379
HaiVL <hai.softech@gmail.com>
2018-01-22 15:41:59 +0700

    finish

--- SimpleProjectRX/SimpleProjectRX/MultipleCellViewController.swift
+++ SimpleProjectRX/SimpleProjectRX/MultipleCellViewController.swift
@@ -22,7 +22,6 @@ class PhotoCollectionViewCell: UICollectionViewCell {
 
     let imageView: UIImageView = {
         let image = UIImageView()
-        image.contentMode = .scaleToFill
         image.contentMode = .scaleAspectFill
         image.backgroundColor = #colorLiteral(red: 1, green: 0.3864146769, blue: 0.4975627065, alpha: 1)
         image.translatesAutoresizingMaskIntoConstraints = false
@@ -49,9 +48,8 @@ class TextCollectionViewCell: UICollectionViewCell {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    let textLabel: UILabel = {
+    var textLabel: UILabel = {
         let label = UILabel()
-        label.text = "label"
         label.textColor = .black
         label.translatesAutoresizingMaskIntoConstraints = true
         return label
@@ -59,9 +57,9 @@ class TextCollectionViewCell: UICollectionViewCell {
 
     func setupView() {
         addSubview(textLabel)
-        backgroundColor = .red
-        textLabel.widthAnchor.constraint(equalToConstant: 50).isActive = true
-        textLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        backgroundColor = .green
+        textLabel.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        textLabel.heightAnchor.constraint(equalToConstant: 100).isActive = true
         textLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
         textLabel.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
     }
@@ -70,22 +68,57 @@ class TextCollectionViewCell: UICollectionViewCell {
 final class MultipleCellViewController: UIViewController {
     @IBOutlet private weak var collectionView: UICollectionView!
 
+    let bag = DisposeBag()
+
+    enum MyCellModel {
+        case text(String)
+        case image(UIImage)
+    }
+
+    let obsevable: Observable<[MyCellModel]> = Observable.of([
+        .text("home"),.image(#imageLiteral(resourceName: "home_1")),
+        .text("info"),.image(#imageLiteral(resourceName: "me_1")),
+        .text("favo"),.image(#imageLiteral(resourceName: "favor_1")),
+        .text("tam"), .text("thu")
+        ])
+
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "multiple cell"
         collectionView.register(TextCollectionViewCell.self, forCellWithReuseIdentifier: "TextCell")
         collectionView.register(PhotoCollectionViewCell.self, forCellWithReuseIdentifier: "PhotoCell")
-        collectionView.dataSource = self
+
+        obsevable.bind(to: collectionView.rx.items) { (collectionView: UICollectionView, index: Int, element: MyCellModel) in
+            let indexPath = IndexPath(item: index, section: 0)
+            switch element {
+            case .text(let title):
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TextCell", for: indexPath)
+                if let cell = cell as? TextCollectionViewCell {
+                    cell.textLabel.text = title
+                }
+                return cell
+
+            case .image(let image):
+                    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath)
+                    if let cell = cell as? PhotoCollectionViewCell {
+                        cell.imageView.image = image
+                }
+                return cell
+            }
+        }.addDisposableTo(bag)
+
+        collectionView.rx.itemSelected
+            .subscribe(onNext: { [weak self] indexPath in
+                guard let this = self else { return }
+                this.showAlert(message: "Items select \(indexPath.row)")
+        }).addDisposableTo(bag)
     }
+
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "demo", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+
 }
 
-extension MultipleCellViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
-    }
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TextCell", for: indexPath)
-        return cell
-    }
-}
