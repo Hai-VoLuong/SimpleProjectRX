4c3d3fd06a33df2f8373975b1e3e461c86f3a63f
HaiVL <hai.softech@gmail.com>
2018-02-27 17:28:05 +0700

    random color

--- SimpleProjectRX/SimpleProjectRX/MagicalGridController.swift
+++ SimpleProjectRX/SimpleProjectRX/MagicalGridController.swift
@@ -13,9 +13,23 @@ class MagicalGridController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Magical Grid to Tinde"
-        let redView = UIView()
-        redView.backgroundColor = .red
-        redView.frame = CGRect(x: 0, y: 64, width: 100, height: 100)
-        view.addSubview(redView)
+
+        let numViewPerRow = 15
+        let width = view.frame.width / CGFloat(numViewPerRow)
+
+        for i in 0...numViewPerRow {
+            let redView = UIView()
+            redView.backgroundColor = randomColor()
+            redView.frame = CGRect(x: CGFloat(i) * width , y: 64, width: width , height: width)
+            view.addSubview(redView)
+        }
+    }
+
+    private func randomColor() -> UIColor {
+        let red = CGFloat(drand48())
+        let green = CGFloat(drand48())
+        let blue = CGFloat(drand48())
+
+        return UIColor(red: red, green: green, blue: blue, alpha: 1)
     }
 }
