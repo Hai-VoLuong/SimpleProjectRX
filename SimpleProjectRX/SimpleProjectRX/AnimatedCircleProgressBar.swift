03838810d6729143809f4acad306ad71150c4981
HaiVL <hai.softech@gmail.com>
2018-03-02 17:13:40 +0700

    init

---
+++ SimpleProjectRX/SimpleProjectRX/AnimatedCircleProgressBar.swift
@@ -0,0 +1,51 @@
+//
+//  AnimatedCircleProgressBar.swift
+//  SimpleProjectRX
+//
+//  Created by Hai Vo L. on 3/2/18.
+//  Copyright © 2018 Hai Vo L. All rights reserved.
+//
+
+import UIKit
+
+final class AnimatedCircleProgressBar: UIViewController {
+
+    let shapeLayer = CAShapeLayer()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Animated Circle Progress Bar"
+
+        let center = view.center
+
+        // create my track layer
+        let trackLayer = CAShapeLayer()
+
+        let circularPath = UIBezierPath(arcCenter: center, radius: 50, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        trackLayer.path = circularPath.cgPath
+
+        trackLayer.strokeColor = UIColor.lightGray.cgColor
+        trackLayer.lineWidth = 10
+        trackLayer.fillColor = UIColor.clear.cgColor
+        trackLayer.lineCap = kCALineCapRound
+        view.layer.addSublayer(trackLayer)
+
+
+        shapeLayer.path = circularPath.cgPath
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        shapeLayer.lineWidth = 10
+        shapeLayer.strokeEnd = 0
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.lineCap = kCALineCapRound
+
+        view.layer.addSublayer(shapeLayer)
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+    }
+
+    func handleTap() {
+       let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.toValue = 1
+        basicAnimation.duration = 2
+        shapeLayer.add(basicAnimation, forKey: "urlSoBasic")
+    }
+}
