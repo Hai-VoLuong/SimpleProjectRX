42e29b018a17bb55efec2642710db4d1e93a3a39
HaiVL <hai.softech@gmail.com>
2018-03-05 15:30:23 +0700

    format code

--- SimpleProjectRX/SimpleProjectRX/AnimatedCircleProgressBar.swift
+++ SimpleProjectRX/SimpleProjectRX/AnimatedCircleProgressBar.swift
@@ -8,49 +8,95 @@
 
 import UIKit
 
+extension UIColor {
+    static func rgb(r: CGFloat, g: CGFloat, b: CGFloat) -> UIColor {
+        return UIColor(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
+
+    static let backgroundColor = UIColor.rgb(r: 21, g: 22, b: 33)
+    static let outlineStrokeColor = UIColor.rgb(r: 234, g: 46, b: 111)
+    static let trackStrokeColor = UIColor.rgb(r: 56, g: 25, b: 49)
+    static let pulsatingFillColor = UIColor.rgb(r: 86, g: 30, b: 63)
+}
+
 class AnimatedCircleProgressBar: UIViewController, URLSessionDownloadDelegate {
-    let shapeLayer = CAShapeLayer()
+
+    var shapeLayer: CAShapeLayer!
+    var pulsatingLayer: CAShapeLayer!
 
     let percentTargeLabel: UILabel = {
         let label = UILabel()
         label.text = "Start"
         label.textAlignment = .center
-        label.font = UIFont.boldSystemFont(ofSize: 32)
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.textColor = .white
         return label
     }()
+
+    // xử lý khi app foreground
+    private func setupNotificationObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleEnterForeGround), name: .UIApplicationWillEnterForeground, object: nil)
+    }
+
+    @objc private func handleEnterForeGround() {
+        animatePulsatingLayer()
+    }
+
+    private func createCircleShapeLayer(strokeColor: UIColor, fillColor: UIColor) -> CAShapeLayer {
+        let layer = CAShapeLayer()
+        let circularPath = UIBezierPath(arcCenter: .zero, radius: 50, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        layer.path = circularPath.cgPath
+        layer.strokeColor = strokeColor.cgColor
+        layer.lineWidth = 10
+        layer.fillColor = fillColor.cgColor
+        layer.lineCap = kCALineCapRound
+        layer.position = view.center
+        return layer
+    }
+
+    fileprivate func setupCircleLayers() {
+        pulsatingLayer = createCircleShapeLayer(strokeColor: .clear, fillColor: .pulsatingFillColor)
+        view.layer.addSublayer(pulsatingLayer)
+
+        animatePulsatingLayer()
+
+        let trackLayer = createCircleShapeLayer(strokeColor: .trackStrokeColor, fillColor: .backgroundColor)
+        view.layer.addSublayer(trackLayer)
+
+        shapeLayer = createCircleShapeLayer(strokeColor: .outlineStrokeColor, fillColor: .clear)
+        shapeLayer.transform = CATransform3DMakeRotation(-CGFloat.pi / 2, 0, 0, 1)
+        view.layer.addSublayer(shapeLayer)
+    }
+
+    fileprivate func setupPercentageLabel() {
+        view.addSubview(percentTargeLabel)
+        percentTargeLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        percentTargeLabel.center = view.center
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Animated Circle Progress Bar"
-
-        view.addSubview(percentTargeLabel)
-        percentTargeLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-        percentTargeLabel.center = view.center
-        // create my track layer
-        let trackLayer = CAShapeLayer()
-
-        let circularPath = UIBezierPath(arcCenter: .zero, radius: 50, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
-        trackLayer.path = circularPath.cgPath
-
-        trackLayer.strokeColor = UIColor.lightGray.cgColor
-        trackLayer.lineWidth = 10
-        trackLayer.fillColor = UIColor.clear.cgColor
-        trackLayer.lineCap = kCALineCapRound
-        trackLayer.position = view.center
-        view.layer.addSublayer(trackLayer)
-
-
-        shapeLayer.path = circularPath.cgPath
-        shapeLayer.strokeColor = UIColor.red.cgColor
-        shapeLayer.lineWidth = 10
-        shapeLayer.strokeEnd = 0
-        shapeLayer.fillColor = UIColor.clear.cgColor
-        shapeLayer.lineCap = kCALineCapRound
-        shapeLayer.position = view.center
-        shapeLayer.transform = CATransform3DMakeRotation(-CGFloat.pi / 2, 0, 0, 1)
-
-        view.layer.addSublayer(shapeLayer)
+        view.backgroundColor = UIColor.backgroundColor
+        setupNotificationObservers()
+        setupCircleLayers()
+        setupPercentageLabel()
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+    }
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+    // làm rung động thở ra thở vô
+    private func animatePulsatingLayer() {
+        let animation = CABasicAnimation(keyPath: "transform.scale")
+        animation.toValue = 1.3
+        animation.duration = 0.8
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        animation.autoreverses = true
+        animation.repeatCount = Float.infinity
+        pulsatingLayer.add(animation, forKey: "pulsing")
     }
 
 
