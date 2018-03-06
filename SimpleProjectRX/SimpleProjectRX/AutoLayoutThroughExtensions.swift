fd6f17e04f8bc5c4b4136332e43ea615938fbef0
HaiVL <hai.softech@gmail.com>
2018-03-06 17:32:58 +0700

    complete

--- SimpleProjectRX/SimpleProjectRX/AutoLayoutThroughExtensions.swift
+++ SimpleProjectRX/SimpleProjectRX/AutoLayoutThroughExtensions.swift
@@ -10,8 +10,31 @@ import UIKit
 
 class AutoLayoutThroughExtensions: UIViewController {
 
+    let redView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        return view
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(redView)
+        title = "auto layout extension"
+        // enable auto layout
+        if #available(iOS 11.0, *) {
+            redView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+}
 
+extension UIView {
+    func anchor(top: NSLayoutYAxisAnchor, leading: NSLayoutXAxisAnchor, bottom: NSLayoutYAxisAnchor, trailing: NSLayoutXAxisAnchor) {
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: top).isActive = true
+        leadingAnchor.constraint(equalTo: leading).isActive = true
+        trailingAnchor.constraint(equalTo: trailing).isActive = true
+        bottomAnchor.constraint(equalTo: bottom).isActive = true
     }
 }
