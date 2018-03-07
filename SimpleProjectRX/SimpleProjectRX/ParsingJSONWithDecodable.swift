3fa97768c4d515c3427375c8e3a92c4ce34b3396
HaiVL <hai.softech@gmail.com>
2018-03-07 16:54:30 +0700

    test parseJson

--- SimpleProjectRX/SimpleProjectRX/ParsingJSONWithDecodable.swift
+++ SimpleProjectRX/SimpleProjectRX/ParsingJSONWithDecodable.swift
@@ -8,10 +8,37 @@
 
 import UIKit
 
+// https://api.letsbuildthatapp.com/jsondecodable/course
+/*
+ {
+ "id": 1,
+ "name": "Instagram Firebase",
+ "link": "https://www.letsbuildthatapp.com/course/instagram-firebase",
+ "imageUrl": "https://letsbuildthatapp-videos.s3-us-west-2.amazonaws.com/04782e30-d72a-4917-9d7a-c862226e0a93"
+ }
+ */
+
+struct Course {
+    let id: Int
+    let name: String
+    let link: String
+    let imageUrl: String
+}
+
 class ParsingJSONWithDecodable: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Parsing JSON With Decodable"
+
+        let jsonString = "https://api.letsbuildthatapp.com/jsondecodable/course"
+
+        guard let url = URL(string: jsonString) else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else { return }
+            let dataString = String(data: data, encoding: .utf8)
+            print(dataString)
+        }.resume()
+
     }
 }
