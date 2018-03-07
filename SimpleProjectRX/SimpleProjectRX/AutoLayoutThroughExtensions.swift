3dceb125f8db5fe05d9459f210de34dfb16e9a54
HaiVL <hai.softech@gmail.com>
2018-03-07 11:06:04 +0700

    complete

--- SimpleProjectRX/SimpleProjectRX/AutoLayoutThroughExtensions.swift
+++ SimpleProjectRX/SimpleProjectRX/AutoLayoutThroughExtensions.swift
@@ -10,31 +10,102 @@ import UIKit
 
 class AutoLayoutThroughExtensions: UIViewController {
 
+    let background: UIView = {
+        let view = UIView()
+        view.backgroundColor = .purple
+        return view
+    }()
+
     let redView: UIView = {
         let view = UIView()
         view.backgroundColor = .red
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 10
+        return view
+    }()
+
+    let blueView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .blue
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 10
+        return view
+    }()
+
+    let greenView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .green
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 10
+        return view
+    }()
+
+    let whiteView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 10
         return view
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(redView)
         title = "auto layout extension"
-        // enable auto layout
+        [background,redView, blueView, greenView, whiteView].forEach({view.addSubview($0)})
+
         if #available(iOS 11.0, *) {
-            redView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
+            background.fillSuperView()
+
+            redView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: nil, bottom: nil, trailing: view.safeAreaLayoutGuide.trailingAnchor, padding: .init(top: 12, left: 0, bottom: 0, right: 12), size: .init(width: 100, height: 0))
+            redView.heightAnchor.constraint(equalTo: redView.widthAnchor).isActive = true
+
+            blueView.anchor(top: redView.bottomAnchor, leading: nil, bottom: nil, trailing: redView.trailingAnchor, padding: .init(top: 12, left: 0, bottom: 0, right: 0))
+            blueView.anchorSize(to: redView)
+
+            greenView.anchor(top: redView.topAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: blueView.bottomAnchor, trailing: redView.leadingAnchor, padding: .init(top: 0, left: 12, bottom: 0, right: 12))
+
+            whiteView.anchor(top: blueView.bottomAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: nil, trailing: blueView.trailingAnchor, padding: .init(top: 12, left: 12, bottom: 0, right: 0), size: .init(width: 0, height: 200))
+
         } else {
-            // Fallback on earlier versions
+
         }
     }
 }
 
 extension UIView {
-    func anchor(top: NSLayoutYAxisAnchor, leading: NSLayoutXAxisAnchor, bottom: NSLayoutYAxisAnchor, trailing: NSLayoutXAxisAnchor) {
+
+    func fillSuperView() {
+        anchor(top: superview?.topAnchor, leading: superview?.leadingAnchor, bottom: superview?.bottomAnchor, trailing: superview?.trailingAnchor)
+    }
+
+    func anchorSize(to view: UIView) {
+        widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+    }
+
+    func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero) {
         translatesAutoresizingMaskIntoConstraints = false
-        topAnchor.constraint(equalTo: top).isActive = true
-        leadingAnchor.constraint(equalTo: leading).isActive = true
-        trailingAnchor.constraint(equalTo: trailing).isActive = true
-        bottomAnchor.constraint(equalTo: bottom).isActive = true
+
+        // fix warning in console
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
+        }
+        if let leading = leading {
+            leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
+        }
+        if let trailing = trailing {
+            trailingAnchor.constraint(equalTo: trailing, constant: -padding.right).isActive = true
+        }
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom).isActive = true
+        }
+
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
     }
 }
