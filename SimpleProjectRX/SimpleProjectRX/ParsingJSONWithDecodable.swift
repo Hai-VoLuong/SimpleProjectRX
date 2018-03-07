fef9c3e4c58c0b5cf77cc92a8997727dc76442cc
HaiVL <hai.softech@gmail.com>
2018-03-07 16:43:40 +0700

    init

---
+++ SimpleProjectRX/SimpleProjectRX/ParsingJSONWithDecodable.swift
@@ -0,0 +1,17 @@
+//
+//  ParsingJSONWithDecodable.swift
+//  SimpleProjectRX
+//
+//  Created by Hai Vo L. on 3/7/18.
+//  Copyright © 2018 Hai Vo L. All rights reserved.
+//
+
+import UIKit
+
+class ParsingJSONWithDecodable: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Parsing JSON With Decodable"
+    }
+}
