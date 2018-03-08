1c3032060dd809c1e94a31b67cef5aa5fca90c4f
HaiVL <hai.softech@gmail.com>
2018-03-08 10:02:03 +0700

    complete

--- SimpleProjectRX/SimpleProjectRX/ParsingJSONWithDecodable.swift
+++ SimpleProjectRX/SimpleProjectRX/ParsingJSONWithDecodable.swift
@@ -35,14 +35,63 @@ import UIKit
      }
  ]
 
+ 3 --> https://api.letsbuildthatapp.com/jsondecodable/website_description
+
+ {
+     "name": "Lets Build That App",
+     "description": "Teaching and Building Apps since 1999",
+     "courses": [
+         {
+             "id": 1,
+             "name": "Instagram Firebase",
+             "link": "https://www.letsbuildthatapp.com/course/instagram-firebase",
+             "imageUrl": "https://letsbuildthatapp-videos.s3-us-west-2.amazonaws.com/04782e30-d72a-4917-9d7a-c862226e0a93"
+         },
+         {
+             "id": 3,
+             "name": "Kindle Basic Training",
+             "link": "https://www.letsbuildthatapp.com/basic-training",
+             "imageUrl": "https://letsbuildthatapp-videos.s3-us-west-2.amazonaws.com/a6180731-c077-46e7-88d5-4900514e06cf"
+         }
+     ]
+ }
+
+ 4 --> https://api.letsbuildthatapp.com/jsondecodable/courses_missing_fields
+
+ [
+     {
+         "id": 1,
+         "name": "Instagram Firebase",
+         "link": "https://www.letsbuildthatapp.com/course/instagram-firebase",
+         "imageUrl": "https://letsbuildthatapp-videos.s3-us-west-2.amazonaws.com/04782e30-d72a-4917-9d7a-c862226e0a93"
+     },
+     {
+         "id": 3,
+         "name": "Kindle Basic Training",
+         "link": "https://www.letsbuildthatapp.com/basic-training",
+         "imageUrl": "https://letsbuildthatapp-videos.s3-us-west-2.amazonaws.com/a6180731-c077-46e7-88d5-4900514e06cf"
+     },
+     {
+         "name": "Yelp"
+     }
+ ]
+
  */
 
-struct Course: Decodable {
-    let id: Int
+struct WebsiteDescription: Decodable {
     let name: String
-    let link: String
-    // let imageUrl: String
+    let description: String
+    let temp: Bool = false
+    let courses: [Course]
 }
+
+struct Course: Decodable {
+    let id: Int?
+    let name: String?
+    let link: String?
+    let imageUrl: String?
+}
+
 
 class ParsingJSONWithDecodable: UIViewController {
 
@@ -52,8 +101,10 @@ class ParsingJSONWithDecodable: UIViewController {
 
         let jsonString1 = "https://api.letsbuildthatapp.com/jsondecodable/course"
         let jsonString2 = "https://api.letsbuildthatapp.com/jsondecodable/courses"
+        let jsonString3 = "https://api.letsbuildthatapp.com/jsondecodable/website_description"
+        let jsonString4 = "https://api.letsbuildthatapp.com/jsondecodable/courses_missing_fields"
 
-        guard let url = URL(string: jsonString2) else { return }
+        guard let url = URL(string: jsonString4) else { return }
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             guard let data = data else { return }
             do {
@@ -62,8 +113,6 @@ class ParsingJSONWithDecodable: UIViewController {
             } catch let err {
                 print("error serializing json, \(err.localizedDescription)")
             }
-
-
         }.resume()
 
     }
