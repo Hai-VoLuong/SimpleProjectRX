e4a28bb78a4f340f579fd2d288f6f968e2bd81a7
HaiVL <hai.softech@gmail.com>
2018-03-13 17:28:06 +0700

    Tracking Current Page and Constraint Animations

--- SimpleProjectRX/SimpleProjectRX/LoginWalkthroughController.swift
+++ SimpleProjectRX/SimpleProjectRX/LoginWalkthroughController.swift
@@ -28,6 +28,28 @@ final class LoginWalkthroughController: UIViewController {
         return [firstPage, secondPage, thirdPage]
     }()
 
+    lazy var pageControl: UIPageControl = {
+        let pc = UIPageControl()
+        pc.pageIndicatorTintColor = .lightGray
+        pc.currentPageIndicatorTintColor = UIColor(red: 247/255, green: 154/255, blue: 27/255, alpha: 1)
+        pc.numberOfPages = self.pages.count + 1
+        return pc
+    }()
+
+    let skipButton: UIButton = {
+        let bt = UIButton(type: .system)
+        bt.setTitle("Skip", for: .normal)
+        bt.setTitleColor(UIColor(red: 247/255, green: 154/255, blue: 27/255, alpha: 1), for: .normal)
+        return bt
+    }()
+
+    let nextButton: UIButton = {
+        let bt = UIButton(type: .system)
+        bt.setTitle("Next", for: .normal)
+        bt.setTitleColor(UIColor(red: 247/255, green: 154/255, blue: 27/255, alpha: 1), for: .normal)
+        return bt
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.isNavigationBarHidden = true
@@ -35,22 +57,67 @@ final class LoginWalkthroughController: UIViewController {
         setupCollectionview()
     }
 
+    var pageControlBottomAnchor: NSLayoutConstraint?
+    var skipAnchor: NSLayoutConstraint?
+    var nextAnchor: NSLayoutConstraint?
+
     fileprivate func setupCollectionview() {
         view.addSubview(collectionView)
-        collectionView.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor)
+        view.addSubview(pageControl)
+        view.addSubview(skipButton)
+        view.addSubview(nextButton)
+
+        pageControlBottomAnchor = pageControl.anchorLayout(nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 40)[1]
+
+        skipAnchor = skipButton.anchorLayout(view.topAnchor, left: view.leftAnchor, bottom: nil, right: nil, topConstant: 16, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 60, heightConstant: 50).first
+
+        nextAnchor = nextButton.anchorLayout(view.topAnchor, left: nil, bottom: nil, right: view.rightAnchor, topConstant: 16, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 60, heightConstant: 50).first
+
+       collectionView.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor)
+
+        regiseterCell()
+    }
+
+    fileprivate func regiseterCell() {
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "loginCell")
         collectionView.register(PageCell.self, forCellWithReuseIdentifier: "cell")
         collectionView.dataSource = self
         collectionView.delegate = self
+    }
+
+    // Tracking Current Page
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let pageCurrent = Int(targetContentOffset.pointee.x / view.frame.width)
+        pageControl.currentPage = pageCurrent
+        if pageCurrent == pages.count {
+            pageControlBottomAnchor?.constant = 40
+            nextAnchor?.constant = -40
+            skipAnchor?.constant = -40
+        } else {
+            pageControlBottomAnchor?.constant = 0
+            nextAnchor?.constant = 16
+            skipAnchor?.constant = 16
+        }
+
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+
     }
 }
 
 extension LoginWalkthroughController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return pages.count
+        return pages.count + 1
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if indexPath.item == pages.count {
+            let loginCell = collectionView.dequeueReusableCell(withReuseIdentifier: "loginCell", for: indexPath)
+            return loginCell
+        }
+
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! PageCell
         cell.page = pages[indexPath.item]
         return cell
