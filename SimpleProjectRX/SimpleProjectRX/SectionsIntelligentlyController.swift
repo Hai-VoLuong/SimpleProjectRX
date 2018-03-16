c81caade5100185b2ba0bb01c83ecc453e3bafac
HaiVL <hai.softech@gmail.com>
2018-03-16 10:54:03 +0700

    tabelView mảng 1 chiều

--- SimpleProjectRX/SimpleProjectRX/SectionsIntelligentlyController.swift
+++ SimpleProjectRX/SimpleProjectRX/SectionsIntelligentlyController.swift
@@ -10,8 +10,63 @@ import UIKit
 
 class SectionsIntelligentlyController: UIViewController {
 
+    fileprivate let cellId = "cell"
+
+    fileprivate let names = ["Army", "Army", "Army", "Army", "Army", "Army", "Army", "Army"]
+    fileprivate let anotherListNames = ["Bary", "Bary", "Bary", "Bary"]
+
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .white
+        tableView.dataSource = self
+        tableView.delegate = self
+        return tableView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavigations()
+        setupUITableView()
+    }
+
+    private func setupNavigations() {
         title = "Sections Intelligently"
+        if #available(iOS 11.0, *) {
+            navigationController?.navigationBar.prefersLargeTitles = true
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+
+    private func setupUITableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        view.addSubview(tableView)
+        tableView.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor)
+    }
+}
+
+extension SectionsIntelligentlyController: UITableViewDataSource, UITableViewDelegate {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        label.text = "Header"
+        label.backgroundColor = .lightGray
+        return label
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return section == 0 ? names.count : anotherListNames.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        var name = self.names[indexPath.row]
+        name = indexPath.section == 0 ? names[indexPath.row] : anotherListNames[indexPath.row]
+        cell.textLabel?.text = name
+        return cell
     }
 }
